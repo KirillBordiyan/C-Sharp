ed29da113e3a3fb8c13b40b4ab397474f8195978
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-04-13 21:40:30 +0300

    dz 2 extend task Integral

--- dz_2/Program.cs
+++ dz_2/Program.cs
@@ -3,14 +3,14 @@
 // 782 -> 8
 // 918 -> 1
 
-int task10() {
-    System.Console.Write("введите трехзначное число: ");
-    int a = int.Parse(System.Console.ReadLine());
-    int num1 = a % 100;
-    int num2 = num1 / 10;
-    System.Console.WriteLine($"{num2}");
-    return num2;
-}
+// int task10() {
+//     System.Console.Write("введите трехзначное число: ");
+//     int a = int.Parse(System.Console.ReadLine());
+//     int num1 = a % 100;
+//     int num2 = num1 / 10;
+//     System.Console.WriteLine($"{num2}");
+//     return num2;
+// }
 
 // task10();
 
@@ -20,26 +20,26 @@ int task10() {
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-void task13(){
-    System.Console.Write("введите число: ");
-    int b = 0;
-    while (! int.TryParse(System.Console.ReadLine(), out b)){
-    System.Console.WriteLine("это не число");
-    }
+// void task13(){
+//     System.Console.Write("введите число: ");
+//     int b = 0;
+//     while (! int.TryParse(System.Console.ReadLine(), out b)){
+//     System.Console.WriteLine("это не число");
+//     }
 
-    int ost = 0;
-    if (b >= 100){
-        do{
-            ost = b % 10;
-            b = b / 10;
-        }
-        while(b >= 100);
-        System.Console.WriteLine(ost);
-    }
-    else{
-        System.Console.WriteLine("число меньше трехзначного");
-    }
-}
+//     int ost = 0;
+//     if (b >= 100){
+//         do{
+//             ost = b % 10;
+//             b = b / 10;
+//         }
+//         while(b >= 100);
+//         System.Console.WriteLine(ost);
+//     }
+//     else{
+//         System.Console.WriteLine("число меньше трехзначного");
+//     }
+// }
 
 // task13();
 
@@ -49,21 +49,50 @@ void task13(){
 // 7 -> да
 // 1 -> нет
 
-void task15(){
-    System.Console.WriteLine("введите число от 1 до 7: ");
-    int day = 0;
-    while ((! int.TryParse(System.Console.ReadLine(), out day) || (day > 7 || day < 1))){
-        System.Console.WriteLine("это число не подходит, введите другое: ");
-    };
+// void task15(){
+//     System.Console.WriteLine("введите число от 1 до 7: ");
+//     int day = 0;
+//     while ((! int.TryParse(System.Console.ReadLine(), out day) || (day > 7 || day < 1))){
+//         System.Console.WriteLine("это число не подходит, введите другое: ");
+//     };
  
-    switch(day){
-        case int i when (i >= 1 && i <=5):
-            System.Console.WriteLine("нет");
-            break;
-        case int i when (i == 6  || i == 7):
-            System.Console.WriteLine("да");
-            break;
+//     switch(day){
+//         case int i when (i >= 1 && i <=5):
+//             System.Console.WriteLine("нет");
+//             break;
+//         case int i when (i == 6  || i == 7):
+//             System.Console.WriteLine("да");
+//             break;
+//     }
+// }
+
+// task15();
+
+// f(x) = kx+g - формула прямой 
+// коэф прямой
+// k = 4 опущена на 
+// g = -4 идет вверх
+// x0 = 10 -начальная точка
+// xN = 20 - конечная точка
+// шаг = 0.0000001
+
+double Integral(){
+    double start = 10;
+    double finish = 20;
+    double step = 0.0000001;
+    
+    double sum = 0;
+    int k = 4;
+    int g = -4;
+
+    while(start <= finish){
+        double h = k * start + g;
+        sum += (step * h);
+        start += step;
     }
+
+    System.Console.WriteLine(sum);
+    return sum;
 }
 
-task15();
+Integral();
