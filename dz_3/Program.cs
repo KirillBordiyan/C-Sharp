8f9f9cf59b5d6f8b67459291c568c75a29e8e1da
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-04-19 22:46:17 +0300

    task 19

--- dz_3/Program.cs
+++ dz_3/Program.cs
@@ -4,3 +4,39 @@
 // 12821 -> да
 // 23432 -> да
 
+int number = ReadNumber();
+
+int ReadNumber()
+{
+    System.Console.WriteLine("введите 5 значное число: ");
+    int num;
+    while (!int.TryParse(System.Console.ReadLine(), out num))
+    {
+        System.Console.Write("еще раз: ");
+    }
+    return num;
+}
+
+
+bool QantOfNum(int num)
+{
+    if(num > 10000 && num <=99999)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+bool Palindrom(int number)
+{
+    string num = number.ToString();
+    return num[0] == num[4] && num[1] == num[3];
+}
+
+
+if (QantOfNum(number)){
+    System.Console.WriteLine(Palindrom(number));
+}else{
+    System.Console.WriteLine("not");
+}
