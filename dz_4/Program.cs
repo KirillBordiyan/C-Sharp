5935194dcd411e0ee689554a44ba4992ff0065d6
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-04-20 21:36:07 +0300

    dz4 task 25

--- dz_4/Program.cs
+++ dz_4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+/*_____________________________________________________________
+int ReadInt(string arg)
+{
+    System.Console.WriteLine($"число {arg}");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("again");
+    }
+    return i;
+}
+
+int a = ReadInt("A: ");
+int b = ReadInt("B: ");
+
+int PowAB(int a, int b)
+{
+    int diff_a = a;
+    for (int i = 1; i < b; i++) // i = 3, a*a*a 1
+    {
+        diff_a *= a;
+    }
+    
+    return diff_a;
+}
+
+System.Console.WriteLine(PowAB(a,b));
+
+___________________________________________________*/
+
