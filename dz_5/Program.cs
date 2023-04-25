a9ea581ea84ba0b254a6211b90f5c03ac2c7494d
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-04-25 19:53:44 +0300

    dz 5 task 36

--- dz_5/Program.cs
+++ dz_5/Program.cs
@@ -60,12 +60,74 @@ int Count_Task34()
     return count;
 }
 
-System.Console.WriteLine(Count_Task34());
+// System.Console.WriteLine(Count_Task34());
 
 
 /*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0*/
+
+//вводим размер массива
+int ReadInt_Task36(string a){ 
+    Console.Write($"input {a}: ");
+
+    int i;
+    while (! int.TryParse(System.Console.ReadLine(), out i)){
+        System.Console.WriteLine("not number");
+    };
+
+    return i;
+}
+
+//заолнить массив:
+void FillArray_Task36(int[] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-100, 100);
+    }
+}
+
+//массив элементов :
+int[] NewArrayEmpty_Task36()
+{
+    int size = ReadInt_Task36("size: ");
+    int[] array = new int[size];
+    FillArray_Task36(array);
+    return array;
+}
+
+//вывод массива: 
+void PrintArray_Task36(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+} 
+
+//сумма эл с нечетными индексами:
+int Summ()
+{
+    int sum = 0;
+    int[] array = NewArrayEmpty_Task36();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1)
+        {
+            sum += array[i];
+        }
+    }
+    PrintArray_Task36(array);
+    return sum;
+}
+
+// System.Console.WriteLine(Summ());
+
 
 
 
