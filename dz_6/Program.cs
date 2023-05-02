2d9d808a843b79ecac85ab574c1799487cfee51a
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-05-02 14:10:16 +0300

    dz 6 task 41

--- dz_6/Program.cs
+++ dz_6/Program.cs
@@ -33,3 +33,24 @@ BiggerThanOu();
 
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+int k1 = ReadInt("k1: ");
+int k2 = ReadInt("k2: ");
+int b1 = ReadInt("b1: ");
+int b2 = ReadInt("b2: ");
+
+
+void PointOfIntersection(int k1, int b1, int k2, int b2)
+{
+    double x = 0;
+    double AB = k1 * x + b1;
+    double CD = k2 * x + b2;
+
+    x = (b2 - b1) / (k1 - k2);
+
+    System.Console.WriteLine($"({x}, {x})");
+}
+
+PointOfIntersection(k1, b1, k2, b2);
+
