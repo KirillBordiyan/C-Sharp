4cca276bee9bc8364d3ad470bc2770180ea227be
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-05-02 15:47:35 +0300

    dz6 task 43 + menu

--- dz_6/Program.cs
+++ dz_6/Program.cs
@@ -2,7 +2,8 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
 
-int ReadInt(string a){ //вводим размер массива
+int ReadInt(string a)
+{
     Console.Write($"input {a}: ");
 
     int i;
@@ -17,6 +18,7 @@ void BiggerThanOu()
 {
     int count = 0;
     Stack<int> arrayOfNumbers = new Stack<int>();
+    System.Console.WriteLine("next or any");
     while (System.Console.ReadLine() == "next")
     {
         arrayOfNumbers.Push(ReadInt("num: "));
@@ -28,29 +30,41 @@ void BiggerThanOu()
     }
     System.Console.WriteLine(count);
 }
-BiggerThanOu();
+
 
 
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
 
-int k1 = ReadInt("k1: ");
-int k2 = ReadInt("k2: ");
-int b1 = ReadInt("b1: ");
-int b2 = ReadInt("b2: ");
-
-
-void PointOfIntersection(int k1, int b1, int k2, int b2)
+void PointOfIntersection()
 {
-    double x = 0;
-    double AB = k1 * x + b1;
-    double CD = k2 * x + b2;
+    int k1 = ReadInt("k1: ");
+    int k2 = ReadInt("k2: ");
+    int b1 = ReadInt("b1: ");
+    int b2 = ReadInt("b2: ");
+    double x = (double) (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
 
-    x = (b2 - b1) / (k1 - k2);
-
-    System.Console.WriteLine($"({x}, {x})");
+    System.Console.WriteLine($"({x}, {y})");
 }
 
-PointOfIntersection(k1, b1, k2, b2);
 
+
+void Menu()
+{
+    System.Console.WriteLine("number tasks - 41 or 43");
+    string task = System.Console.ReadLine();
+
+    switch(task)
+    {
+        case "41": BiggerThanOu();
+        break;
+
+        case "43": PointOfIntersection();
+        break;
+    }
+        
+}
+
+Menu();
