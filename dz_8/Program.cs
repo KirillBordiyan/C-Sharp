588b53a78c0653ea3a30cfe27bc6f0e808eb0b73
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-05-23 19:59:31 +0300

    dz 8 task 56

--- dz_8/Program.cs
+++ dz_8/Program.cs
@@ -69,14 +69,75 @@ void ArraySort(int[,] array)
                     array[i,j] = array[i,k];
                     array[i, k] = temp;
                 }   
-            }
-                
-        } // надо сделать так, чтобы они не менялись местами, а именно перемещались
-
+            }       
+        }
     }
-
-
     Print(array);
 }
 
-ArraySort(array);
+// ArraySort(array);
+
+
+
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int[] arrayTask56 = CreateArraySum(array);
+SummaryByRow(arrayTask56);
+
+bool IsNotSquare(int[,] array) //проверяет, является ли матрица квадратной
+{
+    return array.GetLength(0) != array.GetLength(1);
+}
+
+int[] EmptyArrayOfSumm(int[,] array) // создает пустой массив размерностью == n-строк двумерного массива
+{
+    int[] summary = new int[array.GetLength(0)];
+    foreach(int item in summary)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+    return summary;
+}
+
+int[] CreateArraySum(int[,] array) //заполняет пустой одномерный массив суммами по строкам из двумерного
+{
+    int[] newArray = EmptyArrayOfSumm(array);
+    if(IsNotSquare(array))
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum += array[i,j];
+            }
+            System.Console.WriteLine($"{i+1} -- {sum}");
+            newArray[i] = sum; 
+        }
+    }
+    return newArray;
+}
+
+void SummaryByRow(int[] array) //выводит, какая строка двумерного массива имеет наименьшую сумму элементов (индекс одномерного массива == номеру строки двумерного)
+{
+    int min = 0;
+    for(int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < array[min])
+        {
+            min = i;
+        }
+    }
+    System.Console.WriteLine($"строка минимальной суммы -- {min+1}");
+}
+
+
+
+
